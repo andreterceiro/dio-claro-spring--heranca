c9c04d8da746e75be23fab38537052093f0f62cb
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-26 12:05:09 -0300

    Child class

---
+++ src/Child.java
@@ -0,0 +1,13 @@
+public class Child extends Father{
+    public void publicMethodOfChild() {
+        System.out.println("Public method of child");
+    }
+
+    public void abstractPublicMethodOfFather() {
+        System.out.println("Abstract public method of father");
+    }
+
+    public void publicMethodOfFather() {
+        System.out.println("Public method of father implementd in the Child class");
+    }
+}
