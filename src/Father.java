009865e7ae4bc6035a542c81acd354fe80310209
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-26 12:05:00 -0300

    Father class

---
+++ src/Father.java
@@ -0,0 +1,19 @@
+abstract public class Father {
+    protected void protectedMethodOfFather() {
+        System.out.println("Protected method of father");
+    }
+    abstract protected void abstractPublicMethodOfFather();
+
+    public void publicMethodOfFather() {
+        System.out.println("Public method of father");
+    }
+
+    private void privateMethodOfFather() {
+        System.out.println("Private method of father");
+    }
+    public static void main(String[] args) {
+        Father father = new Father();
+        father.privateMethodOfFather();
+    }
+    
+}
