01f2eac35570c668787fec51a316dde12c9483fa
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-26 12:48:08 -0300

    New test classes

---
+++ src/Father2.java
@@ -0,0 +1,12 @@
+public class Father2 {
+    protected void protectedMethodOfFather2() {
+        System.out.println("Protected method of father 2");
+    }
+    protected void protectedMethod2OfFather2() {
+        System.out.println("Protected method 2 of father 2");
+    }
+    public void publicMethodOfFather2() {
+        System.out.println("Public method of father 2");
+    }
+    
+}
